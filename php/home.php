916ffb8d3d6aa1f68ef0321a2bd57c84cb17f628
silviyayanakieva <shady_lp_96@abv.bg>
2018-06-27 00:20:22 +0300

    +if no session is established -> login

--- php/home.php
+++ php/home.php
@@ -1,7 +1,10 @@
 	
 	<?php
 			session_start();
-			
+			if(!isset($_SESSION['userID']))
+			{
+				header("Location: ../index.php");
+			}
 			$conn=new PDO('mysql:host=localhost;dbname=scdb;charset=utf8', 'root', '');
 			$userid=$_SESSION['userID'];
 			$sql="SELECT * FROM `users` WHERE `userID`='$userid'" ;
--- php/secretaryhome.php
+++ php/secretaryhome.php
@@ -20,45 +20,78 @@
 				$conn=new PDO('mysql:host=localhost;dbname=scdb;charset=utf8', 'root', '');
 				$sql="SELECT * FROM `roomreservation` WHERE `approved`='no'";
 				$result=$conn->query($sql);
-				echo"<table><tr><th>Заявка номер</th><th>Зала</th><th>Дата</th><th>От(час)</th><th>До(час)</th><th>Действие</th></tr>";				
+				echo"<table><tr><th>Заявка номер</th><th>Вид зала</th><th>Дата</th><th>От(час)</th><th>До(час)</th><th>Преподавател</th><th>Състояние</th></tr>";				
 				while($row=$result->fetch())
 				{
 					$requestnumber=$row['reservationID'];
 					$roomtype='';
 					if($row['roomtype']=='auditorium')
 					{
-					 $roomtype='Лекционна'; 
+						$roomtype='Лекционна'; 
 					 }
 					elseif($row['roomtype']=='computer')
 					{
-					 $roomtype='Комп. кабинет'; 
+						$roomtype='Комп. кабинет'; 
 					 }
 					elseif($row['roomtype']=='seminar')
 					{
-					 $roomtype='Семинарна'; 
+						$roomtype='Семинарна'; 
 					 }
 					 else
 					 {
 						$roomtype=$row['roomtype']; 
 					 }
-					 
-					
+					 					 
 					$date=$row['date'];
 					$timefrom=$row['timefrom'];
-					$timeto=$row['timeto'];
-					$approved=$row['approved'];	
-					$secretaryhome="secretaryhome.php";
-					$method="post";
-					$hidden="hidden";
-					$request="request";
-					$submit="submit";
-					
+					$timeto=$row['timeto'];	
+					$teacher=$row['teachernames'];
 															
-						echo"<tr><td>$requestnumber</td><td>$roomtype</td><td>$date</td><td>$timefrom</td><td>$timeto</td>
-						<td><form action='$secretaryhome' method='$method'><input type='$hidden' value='$requestnumber' name='$request'><input type='$submit' value='Обработи'></form></td></tr>";										
+					echo"<tr><td>$requestnumber</td><td>$roomtype</td><td>$date</td><td>$timefrom</td><td>$timeto</td><td>$teacher</td><td>Чакащо</td></tr>";										
 				}
-				echo"</table>";
+				echo"</table>";								
 				?>
+		<h3>Назначи зала:</h3>
+		<form action="secretaryhome.php" method="post">
+			<label for="reservationnumber">Номер на заявка:</label>
+			<input type="text" id="reservationnumber" name="reservationnumber">
+			<label for="assignedroom">Назначена зала:</label>
+			<input type="text" name="assignedroom" id="assignedroom"><br>
+			<label for="approved">Одобрено:</label><br>	
+			<input type="radio" name="approved" value="yes">Да<br>
+			<input type="radio" name="approved" value="no">Не<br>
+			<input type="submit" value="Обнови заявлението">
+		</form>
+		
+		<?php
+			if($_POST)
+			{
+				$conn=new PDO('mysql:host=localhost;dbname=scdb;charset=utf8', 'root', '');				
+				$reservationnumber=$_POST['reservationnumber'];
+				$sql="SELECT * FROM `roomreservation` WHERE `reservationID`='$reservationnumber'";
+				$result=$conn->query($sql);
+				$row=$result->fetch();
+				$assignedroom=$row['roomtype'];
 				
-</body>
+				if(!empty($_POST['assignedroom']))
+				{
+					$assignedroom=$_POST['assignedroom'];
+				}
+				$approved='no';
+				$reviewed='yes';
+				if($_POST['approved']=='yes')
+				{
+					$approved='yes';
+				}
+				
+				$sql="UPDATE `roomreservation`
+					SET `reviewed` = '$reviewed', `approved` = '$approved', `roomtype`='$assignedroom'
+					WHERE `reservationID`='$reservationnumber'";
+					
+				$conn->query($sql) or die('Неуспешнa промяна на заявка заявка! Опитайте отново.');
+				//header("Location: secretaryhome.php");
+			}
+		
+		?>
+	</body>
 </html>
